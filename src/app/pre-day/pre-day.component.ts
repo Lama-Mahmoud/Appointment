8b4965dbf7528837fab4756aac7387031c4c14e5
lama mahmoud <lamamahmoud213@gmail.com>
2023-05-14 01:39:15 +0300

    Editing Weekly dialog view

--- src/app/pre-day/pre-day.component.ts
+++ src/app/pre-day/pre-day.component.ts
@@ -10,27 +10,4 @@ import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class PreDayComponent {
   @Input() day!:IDayNumber;
-  closeResult!: string;
-  
-  constructor(private modalService: NgbModal){}
-  
-  openDialog() {
-    let content=FormComponent;
-    console.log(content)
-    this.modalService.open(content).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
 }
--- src/app/weekly-slots/weekly-slots.component.ts
+++ src/app/weekly-slots/weekly-slots.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ISlots } from '../model/slots.interface';
+import { FormComponent } from '../form/form.component';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-weekly-slots',
@@ -8,5 +10,28 @@ import { ISlots } from '../model/slots.interface';
 })
 export class WeeklySlotsComponent {
   @Input() slots!:ISlots;
+  closeResult!: string;
+  
+  constructor(private modalService: NgbModal){}
+  
+  openDialog() {
+    let content=FormComponent;
+    console.log(content)
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 
 }
