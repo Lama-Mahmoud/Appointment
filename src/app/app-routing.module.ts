f17930b1c7c528f03d5f4af4cd313d1fa0054077
lama mahmoud <lamamahmoud213@gmail.com>
2023-05-15 13:15:10 +0300

    Editing imports in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MonthlyViewComponent } from './monthly-view/monthly-view.component';
-import { WeeklyViewComponent } from './weekly-view/weekly-view.component';
+import { MonthlyViewComponent } from './component/monthly-view/monthly-view.component';
+import { WeeklyViewComponent } from './component/weekly-view/weekly-view.component';
 
 const routes: Routes = [
   {path:"",component:MonthlyViewComponent},
